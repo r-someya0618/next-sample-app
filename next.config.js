49fc56924109669335192219f5356a1da5b46f3f
RyosukeSomeya <ryosuke.someya.0618.dev@gmail.com>
2023-07-04 00:33:26 +0900

    styledComponent追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    // styledComponentsを有効化
+    styledComponents: true
+  }
 }
 
 module.exports = nextConfig
