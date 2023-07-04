85ce28b2691c03a19a19377c06a8ee9c633e43f6
RyosukeSomeya <ryosuke.someya.0618.dev@gmail.com>
2023-07-05 01:00:46 +0900

    jest関連設定, 本番環境での[data-testid]削除

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    // styledComponentsを有効化
-    styledComponents: true,
-  },
+  compiler: (() => {
+    let compilerConfig = {
+      // styledComponentsを有効化
+      styledComponents: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+
+    return compilerConfig
+  })(),
 }
 
 module.exports = nextConfig
