11b912e575285b84d4cd09923b1ab36b64921db6
RyosukeSomeya <ryosuke.someya.0618.dev@gmail.com>
2023-07-13 00:39:29 +0900

    ButtonコンポーネントのStoryを追加

---
+++ src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,77 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Button from './index'
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary'],
+      control: { type: 'radio' },
+      description: 'Button variant',
+      table: {
+        type: { summary: 'primary| secondary' },
+        defaultValue: { summary: 'primary' }
+      }
+    },
+    children: {
+      control: { type: 'text' },
+      description: 'ボタンテキスト',
+      table: {
+        type: { summary: 'string' }
+      }
+    },
+    disabled: {
+      control: { type: 'boolean' },
+      description: 'Disabledフラグ',
+      table: {
+        type: { summary: 'boolean' }
+      }
+    },
+    width: {
+      control: { type: 'number' },
+      description: '横幅',
+      table: {
+        type: { summary: 'number' }
+      }
+    },
+    height: {
+      control: { type: 'number' },
+      description: '高さ',
+      table: {
+        type: { summary: 'number' }
+      }
+    },
+    onClick: {
+      description: 'イベントハンドラ',
+      table: {
+        type: { summary: 'function' }
+      }
+    }
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof Button>
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Primary Button'
+  }
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Secondary Button'
+  }
+}
+
+export const Disabled: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Disabled Button',
+    disabled: true
+  }
+}
